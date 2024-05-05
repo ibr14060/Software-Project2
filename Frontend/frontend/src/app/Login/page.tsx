f803bf08258f72c40746a57e2a5bb66e79506b51
doniahmed18 <seifibr753@gmail.com>
2024-05-05 23:52:15 +0300

    login page setup

--- Frontend/frontend/src/app/Login/page.tsx
+++ Frontend/frontend/src/app/Login/page.tsx
@@ -3,11 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/Login">
-        <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Go to Login
-        </a>
-      </Link>
+      <p>login</p>
     </main>
   );
 }
