4c6a04be4ea5eac2a93193777d388003db595ed9
doniahmed18 <seifibr753@gmail.com>
2024-05-08 04:59:24 +0300

    home but not

--- Frontend/frontend/src/app/HomePage/page.tsx
+++ Frontend/frontend/src/app/HomePage/page.tsx
@@ -1,58 +1,47 @@
-// pages/verify-email.js
 "use client";
 import React from "react";
 import {Button} from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import "./globals.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+type Repo = {
+  name: string
+  stargazers_count: number
+}
 
-export default function VerifyEmail() {
-    const searchParams = useSearchParams();
-    const token = searchParams.get("token");
+export const getServerSideProps = (async () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
 
-console.log("token: ", token);
- 
-const handleSignUp = async () => {
-    try {
-          const response = await fetch('http://localhost:4000/account/confirmsign-up', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ token }),
-      });
-
-      // Handle response
-      if (response.ok) {
-        // Login successful
-        const data = await response.json();
-        console.log(data);
-        const token = data.token;
-        console.log(token);
-        const encodedToken = encodeURIComponent(token);
-
-      } else {
-        // Login failed
-        console.error('Login failed');
-      }
-    } catch (error) {
-      console.error('Error occurred while logging in:', error);
+  const res = await fetch('http://localhost:4000/products/getProducts', {
+    headers: {
+      'Authorization': `Bearer ${token}`
     }
-  };
+  });  const repo: Repo = await res.json()
+  // Pass data to the page via props
+  return { props: { repo } }
+}) satisfies GetServerSideProps<{ repo: Repo }>
 
 
-  return (
-    <div className='b'>
-     <div className="verify">
+export default function HomePage({
+  repo,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+ 
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+console.log("token: ", token);
 
-     <p>Click the button below to verify your email</p>
-     <Button color="success" className="buttonsuccess" onClick={handleSignUp}>
-        Success
-     </Button> 
-     </div> 
 
+return (
+  <div className='b'>
+   <div className="verify">
+
+   <p>Click the button below to verify your email</p>
+   <Button color="success" className="buttonsuccess" >
+      Success
+   </Button> 
+   </div> 
     </div>
-  );
+);
 }
